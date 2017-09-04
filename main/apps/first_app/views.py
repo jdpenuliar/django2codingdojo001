acb035b5ab639eeae109fe081699bb92c7ea2e68
jdpenuliar <jaydz.penuliar@gmail.com>
2017-09-04 09:48:09 -0700

    qs and csrf_exempt

--- main/apps/first_app/views.py
+++ main/apps/first_app/views.py
@@ -2,10 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 import json
 
 # Create your views here.
+@csrf_exempt
 def index(request):
+    print('haha----\n', request)
+    print(request.method)
+    print(request.POST['bar'])
     data = {'val1' : 'this is x', 'val2' : True}
     return HttpResponse(json.dumps(data))
